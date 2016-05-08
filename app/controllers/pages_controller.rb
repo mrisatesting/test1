e99b44ecc14fa42ff750a7bfbc83fc55adb48105
mrisaTest <mrisatest@outlook.com>
2016-05-08 10:29:11 +0200

    init repo test

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def contact
+  end
+
+  def login
+  end
+
+  def signup
+  end
+
+  def about
+  end
+end
